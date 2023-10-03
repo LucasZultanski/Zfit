e53014a3a953d1cfdbbeecfb0d2e7cc2d60e8fa0
LucasZultanski <127425231+LucasZultanski@users.noreply.github.com>
2023-10-03 16:13:01 -0300

    Add files via upload
    fiz a parte de cadastro simples, declarando as suas variáveis

---
+++ main.py
@@ -0,0 +1,11 @@
+# CADASTRO 
+
+print ("INFORMAÇÕES BASICAS:")
+print("Nome:")
+nome = str(input())
+print ("idade:")
+idade = int(input())
+print("Altura:")
+altura = float(input())
+print("Tempo de treino (em minutos):")
+tempo_de_treino = int(input())
